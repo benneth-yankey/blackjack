1f400957f0b8dd1ebb6bc9d93b5c2f0281a312e5
benneth-yankey <benneth.yankey@turntabl.io>
2021-11-18 15:06:01 +0000

    added basic domain models

---
+++ src/main/java/Card.java
@@ -0,0 +1,17 @@
+public class Card {
+    private Face face;
+    private Suit suit;
+
+    public Card(Face face, Suit suit) {
+        this.face = face;
+        this.suit = suit;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "face=" + face +
+                ", suit=" + suit +
+                '}';
+    }
+}
