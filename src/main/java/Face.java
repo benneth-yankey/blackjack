109d8905dde5998564a1f92eeffec0f23f626bfd
benneth-yankey <benneth.yankey@turntabl.io>
2021-11-18 15:48:07 +0000

    added face values

--- src/main/java/Face.java
+++ src/main/java/Face.java
@@ -1,15 +1,27 @@
 public enum Face {
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING,
-    ACE
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10),
+    ACE(11);
+
+    /** Weight representation of the face */
+    private int value;
+
+    private Face(int value){
+        this.value = value;
+    }
+
+    /** Get the value of the card face */
+    public Integer getValue(){
+        return value;
+    }
 }
