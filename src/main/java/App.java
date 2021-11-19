6e5e715e04a72328abf507a45aba05fc26525932
Konadu <cunaedu@gmail.com>
2021-11-19 17:35:14 +0000

    Added game stratergy

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,21 +7,26 @@ public class App {
         System.out.println("blackjack game");
 
         Deck deck = new Deck();
+        Strategy basicStrategy = new Strategy();
 
         // initialize game
-        BlackJack game = new BlackJack(deck, 3);
+        BlackJack game = new BlackJack(deck, basicStrategy, 3);
 
         // start game
         game.play();
 
 
-        System.out.println(game.getPlayers());
+//        System.out.println(game.getPlayers());
 
-        System.out.println();
-        System.out.println( deck.getCards().size());
-        System.out.println("================= after ");
-        System.out.println(deck.popCard());
-        System.out.println(deck.getCards());
-        System.out.println( deck.getCards().size());
+//        game.gameStrategy();
+
+
+
+//        System.out.println();
+//        System.out.println( deck.getCards().size());
+//        System.out.println("================= after ");
+//        System.out.println(deck.popCard());
+//        System.out.println(deck.getCards());
+//        System.out.println( deck.getCards().size());
     }
 }
--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -2,28 +2,91 @@ package io.turntabl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class BlackJack {
     private Deck deck;
     private List<Player> players;
     private int numberOfPlayers;
+    private Strategy strategy;
 
-    public BlackJack(Deck deck, int numberOfPlayers) {
+    public BlackJack(Deck deck, Strategy strategy,  int numberOfPlayers) {
         this.deck = deck;
         this.numberOfPlayers = numberOfPlayers;
+        this.strategy = strategy;
+    }
+
+    /**
+     * display or print players (gui sort of)
+     */
+    public void printStatus() {
+        for (Player player : players) {
+            System.out.println("----- " + player.getName() + " -----");
+            String cards = "Cards: ";
+            for (Card card : player.getCardsInHand()) {
+                Face face = card.getFace();
+                if( face.getValue() == 10 || face.getValue() == 11){
+                    cards += card.getFace()  + " " + card.getSuit() + " ";
+                }else{
+                    cards += card.getFace().getValue() + " " + card.getSuit() + " ";
+                }
+            }
+            System.out.println(cards);
+            System.out.println("Total Weight: " + player.getCardsWeight());
+            System.out.println();
+//    ♣︎ 🖤 💎 ♠\
+        }
     }
 
     /**
      * starts the game
      */
     public void play() {
+        boolean gameIsOn = true;
+        int stickCount = 0;
+
         System.out.println("========== Shuffling ===========");
         deck.shuffleCards();
         createPlayers();
 
         System.out.println("========== Dealing ===========");
         deal();
+
+        // display player cards
+        printStatus();
+
+        // check if any of the players win game
+
+        while (gameIsOn) {
+            for (Player player : players) {
+
+                // Win
+                if (player.getCardsWeight() == 21) {
+                    System.out.println(player.getName() + " has won the game!");
+                    System.out.println("End of game!!");
+                    gameIsOn = false;
+                }
+
+                // Hit
+                else if (player.getCardsWeight() < 17) {
+                    System.out.println(player.getName() + " just hit...");
+                    Card newCard = deck.popCard();
+                    player.setCardsInHand(newCard);
+                    player.setCardsWeight(newCard.getFace().getValue());
+                }
+
+                // Bust
+                else if (player.getCardsWeight() > 21) {
+                    System.out.println(player.getName() + " card weights greater than 21, go bust!!!");
+                    players.remove(player);
+                }
+
+                // Stick
+                else if (player.getCardsWeight() >= 17) {
+                    System.out.println(player.getName() + " just stick...");
+                    stickCount++;
+                }
+            }
+        }
     }
 
     /**
@@ -56,4 +119,26 @@ public class BlackJack {
             }
         }
     }
+
+    /** Game Strategy */
+//    public void gameStrategy(){
+////        boolean playerWon = false;
+//       while (true){
+//           for(Player p : players){
+//               int playerWeight = p.getCardsWeight();
+////               if(playerWeight < 17){
+////                   strategy.hit(p, deck);
+////               }
+////               else if(playerWeight < 21 ){
+////                   strategy.stick(p);
+////               }
+//               if( playerWeight == 21){
+//                   System.out.println("player wins");
+//                   break;
+//               }
+//           }
+//       }
+//    }
+
+
 }
